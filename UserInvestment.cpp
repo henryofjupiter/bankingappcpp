8d630731fd685f633ec55b2ca29c9767f3cd8ea3
henryofjupiter <oshardhenry@gmail.com>
2023-04-01 13:05:39 -0400

    adds code for member functions

--- UserInvestment.cpp
+++ UserInvestment.cpp
@@ -3,3 +3,81 @@
 //
 
 #include "UserInvestment.h"
+#include <string>
+
+string UserInvestment::GetUserName() {
+    return userName;
+}
+void UserInvestment::SetUserName(string name) {
+    this->userName = name;
+}
+unsigned int UserInvestment::GetInitialInvestAmt() {
+
+}
+void UserInvestment::SetInitialInvestAmt() {
+
+}
+unsigned int UserInvestment::GetMonthlyDeposit() {
+
+}
+
+void UserInvestment::SetMonthlyDeposit() {
+
+}
+unsigned int UserInvestment::GetAnnualInterest() {
+
+}
+void UserInvestment::SetAnnualInterest() {
+
+}
+unsigned int UserInvestment::GetNumYears() {
+    return numYears;
+}
+void UserInvestment::SetNumYears(int years) {
+    this->numYears = years;
+}
+
+//calculates sum of initial investment amount and initial monthly deposit.
+double UserInvestment::totalSum(double initialInvestment, double deposit) {
+    double total = initialInvestment + deposit;
+    return total;
+}
+// format string spaces for output
+string formatSpaces(int size, char c) {
+    string newString;
+    for (int i = 0; i < size; ++i) {
+        newString += c;
+    }
+    return newString;
+}
+
+// display for balance without additional monthly deposit
+void UserInvestment::displayFormat1() {
+    cout << "        Balance and Interest Without Additional Monthly Deposits" << endl;
+    cout << "==========================================================================" << endl;
+    cout << " Year" << formatSpaces(13, ' ') << "Year End Balance" << formatSpaces(13, ' ') << "Year End Earned Interest"<<endl;
+    cout << "--------------------------------------------------------------------------" << endl;
+
+}
+
+// header format for menuInput
+void UserInvestment::headerFormat() {
+    cout << formatSpaces(32, '*') << endl;
+    cout << formatSpaces(10, '*')  << " Data Input " << formatSpaces(10, '*') << endl;
+}
+
+//main menu for userinput
+void UserInvestment::menuInput() {
+    headerFormat();
+    cout << "Enter your name: ";
+    cin >> userName;
+    cout << "Initial Investment Amount: ";
+    cin >> initialInvestAmt;
+    cout << "Monthly Deposit: ";
+    cin >> monthlyDeposit;
+    cout << "Annual Interest: ";
+    cin >> annualInterest;
+    cout <<  "Number of Years: ";
+    cin >> numYears;
+
+}
