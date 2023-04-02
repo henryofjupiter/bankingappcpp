b7be56538820c75c50193c3fc3a9a505c4619d26
henryofjupiter <oshardhenry@gmail.com>
2023-04-02 17:46:06 -0400

    adds function to calculate interest from user input. modifies displayFormat2() function, and nest interest function within it.

--- UserInvestment.cpp
+++ UserInvestment.cpp
@@ -59,6 +59,17 @@ void UserInvestment::displayFormat1() {
     cout << "--------------------------------------------------------------------------" << endl;
 
 }
+//display for balance with additional monthly deposits
+void UserInvestment::displayFormat2() {
+    cout << endl;
+    cout << endl;
+    cout << "        Balance and Interest With Additional Monthly Deposits" << endl;
+    cout << "==========================================================================" << endl;
+    cout << " Year" << formatSpaces(13, ' ') << "Year End Balance" << formatSpaces(13, ' ') << "Year End Earned Interest"<<endl;
+    cout << "--------------------------------------------------------------------------" << endl;
+    interest();
+    cout << endl;
+}
 
 // header format for menuInput
 void UserInvestment::headerFormat() {
@@ -79,5 +90,27 @@ void UserInvestment::menuInput() {
     cin >> annualInterest;
     cout <<  "Number of Years: ";
     cin >> numYears;
+
+}
+
+void UserInvestment::interest() {
+  double sumOfTotal = 0, sumOfInvestAmt = 0, sumOfInterest = 0, total = 0, interest;
+  double opening = initialInvestAmt, md = monthlyDeposit;
+    int i;
+    for (i = 1; i <= 5; ++i){
+        for (int j = 1; j <= 12; ++j) {
+            interest = (opening + md) * ((annualInterest / 100)/12);
+            total = opening + md;
+            opening = total + interest;
+
+
+            sumOfInterest += interest;
+
+        }
+
+        sumOfTotal += total;
+        sumOfInvestAmt += opening;
+        cout << i << formatSpaces(18, ' ') << "Total: " << sumOfTotal + sumOfInterest << formatSpaces(20, ' ') <<  sumOfInterest << endl;
+    }
 
 }
