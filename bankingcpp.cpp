ee9f5fb5fae02b1b793164afa27b8b0465f31eab
henryofjupiter <oshardhenry@gmail.com>
2023-04-03 05:30:36 -0400

    refactors and adds inline comments

--- bankingcpp.cpp
+++ bankingcpp.cpp
@@ -8,13 +8,17 @@ using namespace std;
 
 
 int main() {
-    UserInvestment user1;
+    UserInvestment user1; // class declaration for one user.
     string userName;
 
+    // prints menu to user
     user1.menuInput();
+
+    // output user's name
     cout << "User's Name: " << user1.GetUserName() << endl;
-    user1.displayFormat1();
-    user1.displayFormat2();
+
+    user1.displayFormat1(); // static display (no monthly deposit)
+    user1.displayFormat2(); // static display (with monthly deposit)
 
     return 0;
 }
