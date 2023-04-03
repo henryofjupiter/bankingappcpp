b9c8de520f9a8f20d84911c00a9a1928f18e043d
henryofjupiter <oshardhenry@gmail.com>
2023-04-03 01:23:58 -0400

    calls functions from supporting files

--- bankingcpp.cpp
+++ bankingcpp.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include "UserInvestment.h"
+
 using namespace std;
 
+
+
+int main() {
+    UserInvestment user1;
+    string userName;
+
+    user1.menuInput();
+    cout << "User's Name: " << user1.GetUserName() << endl;
+    user1.displayFormat1();
+    user1.displayFormat2();
+
+    return 0;
+}
