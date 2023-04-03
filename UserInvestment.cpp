aa4e54122cfff6c967bcc14a6487d7a3c396cf70
henryofjupiter <oshardhenry@gmail.com>
2023-04-03 05:28:30 -0400

    refactors interest() function fixes logic and calculations.

--- UserInvestment.cpp
+++ UserInvestment.cpp
@@ -10,11 +10,6 @@ string UserInvestment::GetUserName() {
     return userName;
 }
 
-//calculates sum of initial investment amount and initial monthly deposit.
-double UserInvestment::totalSum(double initialInvestment, double deposit) {
-    double total = initialInvestment + deposit;
-    return total;
-}
 // format string spaces for output
 string formatSpaces(int size, char c) {
     string newString;
@@ -69,24 +64,28 @@ void UserInvestment::menuInput() {
 
 }
 
+// calculates both end of year interest and balance, given a number of years from input.
 void UserInvestment::interest(double opening, double monthly, double interest, int years) {
-  double sumOfTotal = 0, sumOfInvestAmt = 0, sumOfInterest = 0, total = 0, newInterest, newOpening = opening;
-    int i;
-    for (i = 1; i <= years; ++i){
-        for (int j = 1; j <= 12; ++j) {
-            newInterest = (newOpening + monthly) * ((interest / 100)/12);
-            total = newOpening + monthly + newInterest;
-            newOpening = total;
+  double sumOfInterest2, sumOfInterest = 0, total = 0, newInterest, balance = opening;
 
+    for (int i = 1; i <= years; ++i){   // calculates interest and balance per year
+        sumOfInterest2 = sumOfInterest; // assigns sum of interest from the past year.
+        for (int j = 1; j <= 12; ++j) {  // calculates interest and balance per month for 12 months
+            newInterest = (balance + monthly) * (interest / 100)/12; // formula for interest balance
+            total = balance + monthly + newInterest;
+            balance = total;
 
             sumOfInterest += newInterest;
 
         }
-
-        sumOfTotal += total;
-        sumOfInvestAmt += newOpening;
-        cout << i << formatSpaces(18, ' ') << "Total: " << fixed << setprecision(2) << sumOfTotal << formatSpaces(20, ' ') <<  sumOfInterest  << endl;
-        newOpening = sumOfTotal;
+        if (i >= 2) {
+            newInterest = sumOfInterest - sumOfInterest2; // current accumulated interest - past year's interest = final interest for the current year
+        }
+        else {
+            newInterest = sumOfInterest;
+        }
+        balance = total;  // balance update: carry over end of year balance to the next year.
+        cout << i << formatSpaces(20, ' ') << fixed << setprecision(2) << balance << formatSpaces(30, ' ') <<  newInterest  << endl;
     }
 
 }
